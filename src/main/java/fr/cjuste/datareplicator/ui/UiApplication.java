1b27f683fabeb8e62f99eb7ca64e3d08be14cf78
Clement Juste <clement.juste@zenika.com>
2014-06-29 11:02:14 +0200

    Ajout de la conf Spring

--- src/main/java/fr/cjuste/datareplicator/ui/UiApplication.java
+++ src/main/java/fr/cjuste/datareplicator/ui/UiApplication.java
@@ -6,27 +6,37 @@ import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
+import javafx.stage.DirectoryChooser;
 import javafx.stage.Stage;
+
+import java.io.File;
+
 
 public class UiApplication extends Application {
 
+    private static final String TITLE = "Réplicateur de données";
+
     @Override
     public void start(final Stage stage) throws Exception {
-        stage.setTitle("Réplicateur de données");
+        stage.setTitle(TITLE);
         stage.centerOnScreen();
 
-        Button btn = new Button("Hello world");
+        Button btn = new Button("Select file");
         btn.setOnAction(new EventHandler<ActionEvent>() {
 
             @Override
             public void handle(final ActionEvent arg0) {
-                System.out.println("Hello World");
+                DirectoryChooser chooser = new DirectoryChooser();
+                chooser.setTitle(TITLE);
+                File defaultDirectory = new File("c:/");
+                chooser.setInitialDirectory(defaultDirectory);
+                File file = chooser.showDialog(stage);
             }
         });
 
         StackPane rootPane = new StackPane();
         rootPane.getChildren().add(btn);
-        stage.setScene(new Scene(rootPane));
+        stage.setScene(new Scene(rootPane, 400, 300));
         stage.show();
     }
 
